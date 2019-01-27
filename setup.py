7e5df77f1b57768ef111fed7ae87b5e56713bb0c
James Prestwich <james@prestwi.ch>
2019-01-27 13:25:50 -0800

    add readme, license, and setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+from setuptools import setup, find_packages
+
+reqs = [
+    'eth-abi==1.3.0',
+    'eth-keys==0.2.1',
+    'eth-account==0.3.0'
+]
+
+setup(
+    name='riemann-ether',
+    version='0.0.1',
+    description=('Transaction creation library for Ethereum'),
+    url='https://github.com/summa-tx/riemann-ether',
+    author='James Prestwich',
+    author_email='james@summa.one',
+    license='LGPLv3.0',
+    install_requires=reqs,
+    packages=find_packages(),
+    package_dir={'ether': 'ether'},
+    keywords = 'ethereum cryptocurrency blockchain development',
+    python_requires='>=3.6'
+)
