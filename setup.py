db12a4a57de89d4907628c19802dd24442204a02
James Prestwich <james@prestwi.ch>
2019-08-29 09:33:21 -0700

    v2.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ reqs = [
 
 setup(
     name='riemann-ether',
-    version='1.3.5',
+    version='2.0.0',
     description=('Transaction creation library for Ethereum'),
     url='https://github.com/summa-tx/riemann-ether',
     author='James Prestwich',
